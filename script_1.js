e3fdced3f7a8563768bbb39c4c212ff42f91e004
LouisKzy <louiskrejezy@gmail.com>
2023-11-16 17:00:51 +0100

    upgrade exo 1

--- script_1.js
+++ script_1.js
@@ -7,4 +7,8 @@ function sayHelloPrenom(string) {
   console.log(`Bonjour ${string} !`);
 }
 let prenom = prompt("Quel est ton prénom ?");
-sayHelloPrenom(prenom);
+if (prenom === "") {
+  console.log("Dommage tu n'as pas de prénom ( belle technique de pensée par lalaine)");
+} else {
+  sayHelloPrenom(prenom);
+}
