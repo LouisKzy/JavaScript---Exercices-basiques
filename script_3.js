92a7721de38df1998e1dbbdeae4f99fc8b3e7a98
LouisKzy <louiskrejezy@gmail.com>
2023-11-16 16:59:40 +0100

    upgrade exo 3

--- script_3.js
+++ script_3.js
@@ -1,4 +1,4 @@
-function triangle(n) {
+function triangleM(n) {
   let result = "";
   let space = " "
   for (let i = 1; i <= n; i++) {
@@ -11,10 +11,25 @@ function triangle(n) {
   }
   return result
 }
-let numberFloor = prompt("Combien d'étage tu veux à ta pyramide de merde.(Tu la mérite tu m'as fait faire une pyramide déja alors rale pas.)");
-if (numberFloor) {
+function triangle(n) {
+  let result = "";
+  let space = " "
+  for (let i = 1; i <= n; i++) {
+    for (let k = n; k > i - 1; k--) {
+      result += space
+    } for (let j = 0; j < i; j++) {
+      result += "#";
+    }
+    result += " \n"
+  }
+  return result
+}
+let numberFloorM = prompt("Combien d'étage tu veux à ta pyramide de merde.(Tu la mérite tu m'as fait faire une pyramide déja alors rale pas.)");
+if (numberFloorM) {
   console.log("PYRAMIDE DE MERDE SUR TOI");
-  console.log(triangle(numberFloor));
+  console.log(triangleM(numberFloorM));
 } else {
   console.log("Bon choix, pas de pyramide de merde pour toi.");
+  let numberFloor = prompt("Ok pas une pyramide de merde mais une normal alors.. Combien d'étages ?");
+  console.log(triangle(numberFloor))
 }
